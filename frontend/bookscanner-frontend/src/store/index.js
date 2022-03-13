84523663aa58f089f995782832ef40df6ef206bd
kce <lock6241@naver.com>
2022-03-13 19:48:37 +0900

    Revert "화면구성 및 store 작성 완료"
    This reverts commit 0da83a96421afdd415fb8d4161560d9e7bbed7d1.

--- frontend/bookscanner-frontend/src/store/index.js
+++ frontend/bookscanner-frontend/src/store/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import Storage from './models/storage'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    Storage
-  }
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
 })
